fe5726b909659b8fb756f9d84f02d783fbc2c2a5
agamrjain <agamrjain@gmail.com>
2016-05-04 12:14:51 +0530

    [AJ] FIRST PUSH

---
+++ Tittle_1_0/src/main/java/com/tittle/Tittle10Application.java
@@ -0,0 +1,12 @@
+package com.tittle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tittle10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tittle10Application.class, args);
+	}
+}
---
+++ Tittle_1_0/src/test/java/com/tittle/Tittle10ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tittle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Tittle10Application.class)
+public class Tittle10ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
